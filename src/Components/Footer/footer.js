757cbf9e336756213e977fa591831ce80d06d935
matiasmaidana25 <matiasmaidana908@gmail.com>
2022-09-24 15:49:02 -0300

    Footer New

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'footer.css';
+
+class footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <>
+                <footer class="footer-secundario">
+                    <div>
+                        <div class="row">
+                            <div class="footer-col">
+                                <h4>Ayuda</h4>
+                                <ul>
+                                    <a href="#/">Contactos</a>
+                                    <a href="#/">Registro</a>
+                                    <a href="#/">Normas</a>
+                                </ul>
+                            </div>
+                            <div class="footer-cool">
+                                <h4>Informacion</h4>
+                                <ul>
+                                    <a href="#/">Politica de privacidad</a>
+                                    <a href="#/">¡Trabaja con nosotros!</a>
+                                    <a href="#/">Preguntas frecuentes</a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </>
+        )
+    }
+}
