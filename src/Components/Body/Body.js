5d6334e51e7a1474efef1ca643b01e353f068890
matiasmaidana25 <matiasmaidana908@gmail.com>
2022-10-05 20:59:51 -0300

    Body and Images

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './body.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 class body extends React.Component {
     constructor(props) {
@@ -9,9 +12,37 @@ class body extends React.Component {
 
     render() {
         return (
-        <body>
+            <body>
+                <div className="d-flex justify-content-around">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Playstation</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
 
-        </body>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="./images/playstation.png" />
+                        <Card.Body>
+                            <Placeholder as={Card.Title} animation="glow">
+                                <Placeholder xs={6} />
+                            </Placeholder>
+                            <Placeholder as={Card.Text} animation="glow">
+                                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                                <Placeholder xs={6} /> <Placeholder xs={8} />
+                            </Placeholder>
+                            <Placeholder.Button variant="primary" xs={6} />
+                        </Card.Body>
+                    </Card>
+                </div>
+                ;
+
+            </body>
         )
     }
 }
